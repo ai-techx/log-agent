15289c6b99af98fc7228248a86471edb0903183b
sirily11 <32106111+sirily11@users.noreply.github.com>
2024-01-25 15:57:23 +0800

    fix: add mongodb support

--- internal/output/map.go
+++ internal/output/map.go
@@ -13,5 +13,10 @@ func GetOutputByConfig[T any](conf config.Output) (Client[T], error) {
 	if conf.Stdout == true {
 		return NewStdout[T](), nil
 	}
+
+	if conf.MongoDB != nil {
+		return NewMongoDBClient[T](*conf.MongoDB), nil
+	}
+
 	return nil, fmt.Errorf("output client not found")
 }
